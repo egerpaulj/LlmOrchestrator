cb8b58b6af0d418aceca04966a0e0b069b654228
egerpaulj <egerpaulj@protonmail.com>
2025-12-30 14:01:07 +0100

    Fix context creation information

--- src/llm_unified_orchestrator/executors/generic_executor.py
+++ src/llm_unified_orchestrator/executors/generic_executor.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel
 from llm_unified_orchestrator.core.prompts.base import PromptProvider
 from llm_unified_orchestrator.core.task import TaskStatus, WorkflowTask, WorkflowTaskTemplate
 from llm_unified_orchestrator.core.workflow import Workflow
-from llm_unified_orchestrator.executors.task_finalizer import StepFinalizer
+from llm_unified_orchestrator.executors.task_finalizer import Finalizer
 from llm_unified_orchestrator.inference_api.llm_config import LlmConfig
 from llm_unified_orchestrator.inference_api.strategies.base import LLMProviderStrategy
 from llm_unified_orchestrator.inference_api.strategies.strategy_factory import create_inference_strategy_local
 
 class WorkflowExecutor:
     
-    def __init__(self, llm_config: LlmConfig, prompt_provider: PromptProvider, task_finlizaer: StepFinalizer):
+    def __init__(self, llm_config: LlmConfig, prompt_provider: PromptProvider, task_finlizaer: Finalizer):
         self.llm_config = llm_config
         self.prompt_provider=prompt_provider
         self.finalizer=task_finlizaer
@@ -35,43 +35,60 @@ class WorkflowExecutor:
     def execute_workflow(self, workflow: Workflow, **kwargs):
         previous = None
         
-        for task in workflow.tasks:
+        for index, task in enumerate(workflow.tasks):
             if task.status is TaskStatus.COMPLETED:
                 continue
             
             llm_inference = create_inference_strategy_local(name = task.template.strategy, llm_config=self.llm_config) # type: ignore
-            self.execute_task(previous=previous, task=task, llm_inference=llm_inference, **kwargs)
-            self.finalizer.finalize(task, workflow)
+            self.execute_task(task_index=index, previous=previous, task=task, llm_inference=llm_inference, **kwargs)
+            self.finalizer.finalize_task(task, workflow)
 
             previous = task
             
-    def execute_task(self, previous: WorkflowTask | None, task: WorkflowTask, llm_inference: LLMProviderStrategy, **kwargs):
-        system_prompt = self.create_system_prompt(previous, task)
+        self.finalizer.finalize_workflow(workflow=workflow)
+            
+    def execute_task(self, task_index: int, previous: WorkflowTask | None, task: WorkflowTask, llm_inference: LLMProviderStrategy, **kwargs):
+        if task.template is None:
+            raise ValueError("The template is not set for the task. Template name: " + task.template_name)
+        
+        system_prompt = self.create_system_prompt(previous_task=previous, task=task, task_index=task_index)
+        user_prompt = self.get_user_prompt(task.template, **kwargs)
+        
+        task.template.system_prompt_default = system_prompt
+        task.template.user_prompt_default = user_prompt
             
         result = llm_inference.inference(
-                model=task.template.model,  # type: ignore
-                prompt=self.get_user_prompt(task.template, **kwargs), # type: ignore
+                model=task.template.model,  
+                prompt= user_prompt,
                 system=system_prompt, 
                 json_response_type=self.get_model_class(task.result_module_name, task.result_class_name))
         
         task.result = result.model_dump_json()
 
-    def create_system_prompt(self, previous: WorkflowTask | None, task: WorkflowTask):
-        system_prompt = self.get_system_prompt(task.template) # type: ignore
+    def create_system_prompt(self, previous_task: WorkflowTask | None, task: WorkflowTask, task_index: int):
+        if task.template is None:
+            raise ValueError("The template is not set for the task. Template name: " + task.template_name)
         
-        if previous is not None:
-            if previous.result is None:
+        system_prompt = self.get_system_prompt(task.template)
+        
+        if previous_task is not None:
+            if previous_task.result is None:
                 raise ValueError("The previous task did not have results")
             
-            task.context = {
-                "PreviousTask": f"{previous.template.name}_{previous.template.description}", # type: ignore
-                "PreviousTask_Result": previous.result
-            }
+            if previous_task.template is None:
+                raise ValueError("The template is not set for the task. Template name: " + previous_task.template_name)
+            
+            task.context = previous_task.context.copy()
+            
+            key = f"PreviousTask_{task_index}_{previous_task.template.name.replace(' ', '')}"
+            task.context.update({f"{key}_name": f"{previous_task.template.name}"})
+            task.context.update({f"{key}_desription": f"{previous_task.template.description}"})
+            task.context.update({f"{key}_result": previous_task.result})
             
             system_prompt = f"""{system_prompt}
             
             ## Context
-            If relevant, use the following information:
+            If relevant, use the following contextual information:
             {task.context}"""
             
         return system_prompt
