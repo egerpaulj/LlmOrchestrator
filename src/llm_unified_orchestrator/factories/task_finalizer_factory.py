8552f4f309d36477b9e879613d95d94b7b299f6b
egerpaulj <egerpaulj@protonmail.com>
2025-12-30 14:01:33 +0100

    finalize the workflow

--- src/llm_unified_orchestrator/factories/task_finalizer_factory.py
+++ src/llm_unified_orchestrator/factories/task_finalizer_factory.py
@@ -1,10 +1,10 @@
 from llm_unified_orchestrator.data_store.repository import WorkflowRepository
-from llm_unified_orchestrator.executors.task_finalizer import PersistentStepFinalizer, StepFinalizer
+from llm_unified_orchestrator.executors.task_finalizer import PersistentStepFinalizer, Finalizer
 
 class TaskFinalizerFactory:
 
     def __init__(self, workflow_repository: WorkflowRepository ):
         self.workflow_repository = workflow_repository
 
-    def create(self) -> StepFinalizer:
+    def create(self) -> Finalizer:
         return PersistentStepFinalizer(workflow_repository=self.workflow_repository)
